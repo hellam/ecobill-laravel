2f510cba4aa88b97e0e9521f3d5559ffe1781e3d
Shiva <hellamixon@gmail.com>
2022-09-12 21:55:34 +0300

    v1.0.0

--- resources/views/user/setup/roles.blade.php
+++ resources/views/user/setup/roles.blade.php
@@ -65,49 +65,58 @@
             <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-5 g-xl-9">
                 <!--begin::Col-->
                 @foreach($roles as $role)
-                <div class="col-md-4">
-                    <!--begin::Card-->
-                    <div class="card card-flush h-md-100">
-                        <!--begin::Card header-->
-                        <div class="card-header">
-                            <!--begin::Card title-->
-                            <div class="card-title">
-                                <h2>{{$role->name}}</h2>
-                            </div>
-                            <!--end::Card title-->
-                        </div>
-                        <!--end::Card header-->
-                        <!--begin::Card body-->
-                        <div class="card-body pt-1">
-                            <!--begin::Users-->
-{{--                            <div class="fw-bolder text-gray-600 mb-5">Total users with this role: 5</div>--}}
-                            <!--end::Users-->
-                            <!--begin::Permissions-->
-                            <div class="d-flex flex-column text-gray-600">
-                                <div class="d-flex align-items-center py-2">
-                                    <span class="bullet bg-primary me-3"></span>All Admin Controls
+                    <div class="col-md-4">
+                        <!--begin::Card-->
+                        <div class="card card-flush h-md-100">
+                            <!--begin::Card header-->
+                            <div class="card-header">
+                                <!--begin::Card title-->
+                                <div class="card-title">
+                                    <h2>{{$role->name}}</h2>
                                 </div>
+                                <!--end::Card title-->
+                            </div>
+                            <!--end::Card header-->
+                            <!--begin::Card body-->
+                            <div class="card-body pt-1">
+                                <!--begin::Users-->
+                                {{--                            <div class="fw-bolder text-gray-600 mb-5">Total users with this role: 5</div>--}}
+                                <!--end::Users-->
+                                <!--begin::Permissions-->
+                                @php($permissions = explode(',',$role->permissions))
+                                @php($counter = 0)
+                                <div class="d-flex flex-column text-gray-600">
+                                    @foreach($permissions as $permission)
+                                        @php($perm = \App\Models\Permission::where('code', $permission)->first())
+                                        @php($counter += 1)
+                                        <div class="d-flex align-items-center py-2">
+                                            <span class="bullet bg-primary me-3"></span>{{$perm->name}}
+                                        </div>
+                                        @break($counter==5)
+                                    @endforeach
 
-                                <div class='d-flex align-items-center py-2'>
-                                    <span class='bullet bg-primary me-3'></span>
-                                    <em>and 7 more...</em>
+                                    @if(count($permissions)>5)
+                                        <div class='d-flex align-items-center py-2'>
+                                            <span class='bullet bg-primary me-3'></span>
+                                            <em>and {{count($permissions)-5}} more...</em>
+                                        </div>
+                                    @endif
                                 </div>
+                                <!--end::Permissions-->
                             </div>
-                            <!--end::Permissions-->
+                            <!--end::Card body-->
+                            <!--begin::Card footer-->
+                            <div class="card-footer flex-wrap pt-0">
+                                <a href="#"
+                                   class="btn btn-light btn-active-primary my-1 me-2">View Role</a>
+                                <button type="button" class="btn btn-light btn-active-light-primary my-1"
+                                        data-bs-toggle="modal" data-bs-target="#kt_modal_update_role">Edit Role
+                                </button>
+                            </div>
+                            <!--end::Card footer-->
                         </div>
-                        <!--end::Card body-->
-                        <!--begin::Card footer-->
-                        <div class="card-footer flex-wrap pt-0">
-                            <a href="#"
-                               class="btn btn-light btn-active-primary my-1 me-2">View Role</a>
-                            <button type="button" class="btn btn-light btn-active-light-primary my-1"
-                                    data-bs-toggle="modal" data-bs-target="#kt_modal_update_role">Edit Role
-                            </button>
-                        </div>
-                        <!--end::Card footer-->
+                        <!--end::Card-->
                     </div>
-                    <!--end::Card-->
-                </div>
                 @endforeach
                 <!--end::Col-->
 

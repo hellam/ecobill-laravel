a9629d87f140115af5ba622a8215d56da39acd87
Shiva <hellamixon@gmail.com>
2022-09-12 21:30:15 +0300

    v1.0.0

--- resources/views/user/setup/roles.blade.php
+++ resources/views/user/setup/roles.blade.php
@@ -64,7 +64,7 @@
             <!--begin::Row-->
             <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-5 g-xl-9">
                 <!--begin::Col-->
-                @for
+                @foreach($roles as $role)
                 <div class="col-md-4">
                     <!--begin::Card-->
                     <div class="card card-flush h-md-100">
@@ -72,7 +72,7 @@
                         <div class="card-header">
                             <!--begin::Card title-->
                             <div class="card-title">
-                                <h2>Administrator</h2>
+                                <h2>{{$role->name}}</h2>
                             </div>
                             <!--end::Card title-->
                         </div>
@@ -80,26 +80,14 @@
                         <!--begin::Card body-->
                         <div class="card-body pt-1">
                             <!--begin::Users-->
-                            <div class="fw-bolder text-gray-600 mb-5">Total users with this role: 5</div>
+{{--                            <div class="fw-bolder text-gray-600 mb-5">Total users with this role: 5</div>--}}
                             <!--end::Users-->
                             <!--begin::Permissions-->
                             <div class="d-flex flex-column text-gray-600">
                                 <div class="d-flex align-items-center py-2">
                                     <span class="bullet bg-primary me-3"></span>All Admin Controls
                                 </div>
-                                <div class="d-flex align-items-center py-2">
-                                    <span class="bullet bg-primary me-3"></span>View and Edit Financial
-                                    Summaries
-                                </div>
-                                <div class="d-flex align-items-center py-2">
-                                    <span class="bullet bg-primary me-3"></span>Enabled Bulk Reports
-                                </div>
-                                <div class="d-flex align-items-center py-2">
-                                    <span class="bullet bg-primary me-3"></span>View and Edit Payouts
-                                </div>
-                                <div class="d-flex align-items-center py-2">
-                                    <span class="bullet bg-primary me-3"></span>View and Edit Disputes
-                                </div>
+
                                 <div class='d-flex align-items-center py-2'>
                                     <span class='bullet bg-primary me-3'></span>
                                     <em>and 7 more...</em>
@@ -120,6 +108,7 @@
                     </div>
                     <!--end::Card-->
                 </div>
+                @endforeach
                 <!--end::Col-->
 
                 <!--begin::Add new card-->

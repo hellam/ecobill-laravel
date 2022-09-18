5cdade5d3946d1df73b4947fad13d66e7084b4e4
Ibrahim <ibrahellamixon@gmail.com>
2022-09-18 13:19:14 +0300

    v1.0.0

--- app/Http/Controllers/User/Reports/AuditTrailController.php
+++ app/Http/Controllers/User/Reports/AuditTrailController.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
-class AuiditTrailController extends Controller
+class AuditTrailController extends Controller
 {
     public function index(): Factory|View|Application
     {

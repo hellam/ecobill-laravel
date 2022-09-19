6c7dcb514ce93f71e34de2560777827c535db9b2
Ibrahim <ibrahellamixon@gmail.com>
2022-09-19 14:00:58 +0300

    v1.0.0

--- app/Http/Controllers/User/Setup/MakerCheckerRulesController.php
+++ app/Http/Controllers/User/Setup/MakerCheckerRulesController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers\User\Setup;
 
 use App\Http\Controllers\Controller;
 use App\Models\MakerCheckerRule;
-use App\Models\MakerCheckerTrx;
 use App\Models\Permission;
-use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
@@ -30,6 +28,7 @@ class MakerCheckerRulesController extends Controller
         $maker_checker_count = MakerCheckerRule::count();
         return view('user.setup.maker_checker', compact('maker_checker_count'));
     }
+
     //Data table API
     public function dt_api(Request $request): JsonResponse
     {
@@ -37,7 +36,7 @@ class MakerCheckerRulesController extends Controller
         return (new DataTables)->eloquent($audit_trail)
             ->addIndexColumn()
             ->editColumn('maker_type', function ($row) {
-                return $row->status==0 ? 'Single Maker Checker' : 'Double Maker Checker';
+                return $row->status == 0 ? 'Single Maker Checker' : 'Double Maker Checker';
             })->editColumn('permission_code', function ($row) {
                 return Permission::where('code', $row->permission_code)->first()->name;
             })->editColumn('created_at', function ($row) {

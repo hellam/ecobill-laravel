0c542737ad3fcaa41dbcfca6da98d37787180211
Ibrahim <ibrahellamixon@gmail.com>
2022-09-20 15:44:14 +0300

    v1.0.0

--- resources/views/user/setup/maker_checker_rules.blade.php
+++ resources/views/user/setup/maker_checker_rules.blade.php
@@ -126,7 +126,7 @@
                         <thead>
                         <!--begin::Table row-->
                         <tr class="text-start text-gray-400 fw-bolder fs-7 text-uppercase gs-0">
-                            <th class="min-w-125px">#</th>
+                            <th class="min-w-50px">#</th>
                             <th class="min-w-125px">{{__('messages.type')}}</th>
                             <th class="min-w-125px">{{__('messages.action')}}</th>
                             <th class="min-w-125px">{{__('messages.created_by')}}</th>
@@ -302,6 +302,132 @@
             </div>
         </div>
         <!--end::Modal Rule - Add-->
+        <!--begin::Modal Rule - Edit-->
+        <div class="modal fade" id="kt_modal_update_rule" tabindex="-1" aria-hidden="true" data-backdrop='static'
+             data-keyboard='false'>
+            <!--begin::Modal dialog-->
+            <div class="modal-dialog modal-dialog-centered mw-650px">
+                <!--begin::Modal content-->
+                <div class="modal-content">
+                    <!--begin::Form-->
+                    <form class="form" action="#" id="kt_modal_update_rule_form"
+                          data-kt-action="{{route('user.setup.maker_checker_rules.create')}}"
+                          data-kt-redirect="{{route('user.setup.maker_checker_rules.all')}}">
+                        <!--begin::Modal header-->
+                        <div class="modal-header" id="kt_modal_update_rule_header">
+                            <!--begin::Modal title-->
+                            <h2 class="fw-bolder">{{__('messages.new').' '.__('messages.maker_checker_rule')}}</h2>
+                            <!--end::Modal title-->
+                            <!--begin::Close-->
+                            <div id="kt_modal_update_rule_close"
+                                 class="btn btn-icon btn-sm btn-active-icon-primary">
+                                <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
+                                <span class="svg-icon svg-icon-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                         height="24" viewBox="0 0 24 24" fill="none">
+                                        <rect opacity="0.5" x="6" y="17.3137" width="16"
+                                              height="2" rx="1"
+                                              transform="rotate(-45 6 17.3137)"
+                                              fill="currentColor"/>
+                                        <rect x="7.41422" y="6" width="16" height="2" rx="1"
+                                              transform="rotate(45 7.41422 6)"
+                                              fill="currentColor"/>
+                                    </svg>
+                                </span>
+                                <!--end::Svg Icon-->
+                            </div>
+                            <!--end::Close-->
+                        </div>
+                        <!--end::Modal header-->
+                        <!--begin::Modal body-->
+                        <div class="modal-body py-10 px-lg-17">
+                            <!--begin::Scroll-->
+                            <div class="scroll-y me-n7 pe-7" id="kt_modal_update_rule_scroll" data-kt-scroll="true"
+                                 data-kt-scroll-activate="{default: false, lg: true}"
+                                 data-kt-scroll-max-height="auto"
+                                 data-kt-scroll-dependencies="#kt_modal_update_rule_header"
+                                 data-kt-scroll-wrappers="#kt_modal_update_rule_scroll"
+                                 data-kt-scroll-offset="300px">
+                                <!--begin::Input group-->
+                                <div class="row g-9 mb-7">
+                                    <!--begin::Col-->
+                                    <div class="col-md-6 fv-row">
+                                        <!--begin::Label-->
+                                        <label
+                                            class="required fs-6 fw-bold mb-2">{{__('messages.permission')}}</label>
+                                        <!--end::Label-->
+                                        <!--begin::Input-->
+                                        <select class="form-select form-select-solid fw-bolder"
+                                                data-kt-select2="true"
+                                                name="action"
+                                                id="permissions_select"
+                                                data-placeholder="Select Permission" data-allow-clear="true"
+                                                data-kt-audit-trail-table-filter="type"
+                                                data-dropdown-parent="#kt_modal_update_rule">
+                                            <option></option>
+                                            @foreach($permissions as $permission)
+                                                <option value="{{$permission->code}}">{{$permission->name}}</option>
+                                            @endforeach
+                                            <option></option>
+                                        </select>
+                                        <span id="action"></span>
+                                        <!--end::Input-->
+                                    </div>
+                                    <!--end::Col-->
+                                    <!--begin::Col-->
+                                    <div class="col-md-6 fv-row" id="maker_type">
+                                        <!--begin::Label-->
+                                        <label
+                                            class="required fs-6 fw-bold mb-2">{{__('messages.type')}}</label>
+                                        <!--end::Label-->
+                                        <!--begin::Options-->
+                                        <div class="d-flex align-items-center mt-3">
+                                            <!--begin::Option-->
+                                            <label class="form-check form-check-inline form-check-solid me-5">
+                                                <input class="form-check-input" name="maker_type" type="radio"
+                                                       value="0" checked id="maker_type1"/>
+                                                <span class="fw-semibold ps-2 fs-6">{{__('messages.single')}}</span>
+                                            </label>
+                                            <!--end::Option-->
+                                            <!--begin::Option-->
+                                            <label class="form-check form-check-inline form-check-solid">
+                                                <input class="form-check-input" name="maker_type" type="radio"
+                                                       value="1"/>
+                                                <span class="fw-semibold ps-2 fs-6">{{__('messages.double')}}</span>
+                                            </label>
+                                            <!--end::Option-->
+                                        </div>
+                                    </div>
+                                    <!--end::Col-->
+                                </div>
+                                <!--end::Input group-->
+                            </div>
+                            <!--end::Scroll-->
+                        </div>
+                        <!--end::Modal body-->
+                        <!--begin::Modal footer-->
+                        <div class="modal-footer flex-center">
+                            <!--begin::Button-->
+                            <button type="reset" id="kt_modal_update_rule_cancel" class="btn btn-light me-3">
+                                Discard
+                            </button>
+                            <!--end::Button-->
+                            <!--begin::Button-->
+                            <button type="submit" id="kt_modal_update_rule_submit" class="btn btn-primary">
+                                <span class="indicator-label">Submit</span>
+                                <span class="indicator-progress">Please wait...
+                                    <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                                    </span>
+                            </button>
+                            <!--end::Button-->
+                        </div>
+                        <!--end::Modal footer-->
+                    </form>
+                    <!--end::Form-->
+                </div>
+            </div>
+        </div>
+        <!--end::Modal Rule - Edit-->
         <!--end::Modals-->
     </div>
     <!--end::Container-->

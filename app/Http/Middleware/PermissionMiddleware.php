189165e3428a35dc5a7b11a6856681323e897ada
Hellix <hellamixon@gmail.com>
2022-09-23 14:21:21 +0300

    v1.0.0

--- app/Http/Middleware/PermissionMiddleware.php
+++ app/Http/Middleware/PermissionMiddleware.php
@@ -24,29 +24,29 @@ class PermissionMiddleware
      * @param $permission_code
      * @return mixed
      */
-    public function handle(Request $request, Closure $next, $permission_code,$trx_type='')
+    public function handle(Request $request, Closure $next, $permission_code, $trx_type = '')
     {
         if (Auth::guard('user')->check() && check_permission($permission_code)) {
             $maker_checker = requires_maker_checker($permission_code);
             if (!$request->isMethod("GET") && is_array($maker_checker)) {
-                if (!$request->has('remarks'))
-                    return error_web_processor(
-                        __('messages.msg_remarks_required'),
-                        203
-                    );
                 if ($maker_checker[1] != null) {
                     $validator = app()->call([UserValidators::class, $maker_checker[1]]);
                     if ($validator != '') {
                         return $validator;
                     }
                 }
+                if (!$request->has('remarks'))
+                    return error_web_processor(
+                        __('messages.msg_remarks_required'),
+                        203
+                    );
                 return app()
                     ->call([MakerCheckerTrxController::class, 'create'],
-                    [
-                        'mc_type' => $maker_checker[0],
-                        'module'=>$maker_checker[2],
-                        'trx_type' => $trx_type,
-                    ]);
+                        [
+                            'mc_type' => $maker_checker[0],
+                            'module' => $maker_checker[2],
+                            'trx_type' => $trx_type,
+                        ]);
             }
             return $next($request);
         }

2b89d737c54f94fccfd4ef2d6f1e9f012d66510d
Hellix <hellamixon@gmail.com>
2022-09-24 10:16:49 +0300

    v1.0.0

--- routes/user.php
+++ routes/user.php
@@ -81,7 +81,7 @@ Route::group(['as' => 'user.'], function () {
             Route::controller(User\Utils\MakerCheckerTrxController::class)->prefix('unsupervised-data')->as('unsupervised_data.')->group(function () {
                 Route::get('/', 'index')->name('all')->middleware('permission:801');
                 Route::get('/dt_api', 'dt_api')->name('dt_api')->middleware('permission:801');
-                Route::post('/update/{id}', 'update')->name('update')->middleware('permission:802');
+                Route::post('/update/{id}/{action}', 'update')->name('update')->middleware('permission:802');
             });
         });
 

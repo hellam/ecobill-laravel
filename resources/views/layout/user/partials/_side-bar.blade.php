e6872ea30cc70da9b7a2b3d5088bcbb682da8ed8
Ibrahim <ibrahellamixon@gmail.com>
2022-09-24 17:39:43 +0300

    v1.0.0

--- resources/views/layout/user/partials/_side-bar.blade.php
+++ resources/views/layout/user/partials/_side-bar.blade.php
@@ -371,6 +371,18 @@
                         <!--begin:Menu item-->
                         <div class="menu-item">
                             <!--begin:Menu link-->
+                            <a class="menu-link @if(Route::is('user.setup.branches.all'))active @endif" href="{{route('user.setup.branches.all')}}">
+												<span class="menu-bullet">
+													<span class="bullet bullet-dot"></span>
+												</span>
+                                <span class="menu-title">{{__('messages.branches')}}</span>
+                            </a>
+                            <!--end:Menu link-->
+                        </div>
+                        <!--end:Menu item-->
+                        <!--begin:Menu item-->
+                        <div class="menu-item">
+                            <!--begin:Menu link-->
                             <a class="menu-link" href="#">
 												<span class="menu-bullet">
 													<span class="bullet bullet-dot"></span>
--- resources/views/user/setup/branches.blade.php
+++ resources/views/user/setup/branches.blade.php
@@ -1,1 +1,46 @@
-<?php
+@extends('layout.user.app')
+@section('title', 'Branches')
+@section('page_title')
+    <!--begin::Page title-->
+    <div class="page-title d-flex justify-content-center flex-column me-5">
+        <!--begin::Title-->
+        <h1 class="d-flex flex-column text-dark fw-bold fs-3 mb-0">{{__('messages.branches')}}</h1>
+        <!--end::Title-->
+        <!--begin::Breadcrumb-->
+        <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 pt-1">
+            <!--begin::Item-->
+            <li class="breadcrumb-item text-muted">
+                <a href="{{route('user.dashboard')}}" class="text-muted text-hover-primary">{{__('messages.home')}}</a>
+            </li>
+            <!--end::Item-->
+            <!--begin::Item-->
+            <li class="breadcrumb-item">
+                <span class="bullet bg-gray-200 w-5px h-2px"></span>
+            </li>
+            <!--end::Item-->
+            <!--begin::Item-->
+            <li class="breadcrumb-item text-muted">{{__('messages.setup')}}</li>
+            <!--end::Item-->
+            <!--begin::Item-->
+            <li class="breadcrumb-item">
+                <span class="bullet bg-gray-200 w-5px h-2px"></span>
+            </li>
+            <!--end::Item-->
+            <!--begin::Item-->
+            <li class="breadcrumb-item text-dark">{{__('messages.branches')}}</li>
+            <!--end::Item-->
+        </ul>
+        <!--end::Breadcrumb-->
+    </div>
+    <!--end::Page title-->
+@stop
+@section('content')
+    <!--begin::Container-->
+    <div id="kt_content_container" class="container-xxl">
+    </div>
+    <!--end::Container-->
+@stop
+
+@push('custom_scripts')
+    <script src="{{ asset('assets/js/pages/branches/list.js') }}"></script>
+@endpush

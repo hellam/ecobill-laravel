99be18b76348188530b5bc9255e45c2989c72ca0
Hellix <hellamixon@gmail.com>
2022-09-25 14:43:26 +0300

    v1.0.0

---
+++ database/migrations/2022_09_25_143131_create_currency_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currency', function (Blueprint $table) {
+            $table->id();
+            $table->string('abbreviation',20);
+            $table->string('symbol',20);
+            $table->string('name',100);
+            $table->string('hundredths_name',20);
+            $table->string('country',100);
+            $table->boolean('auto_fx')->nullable()->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currency');
+    }
+};

b458ba9cc448e498f16f49483e180e13a484458d
Hellix <hellamixon@gmail.com>
2022-09-27 17:24:39 +0300

    v1.0.0

--- app/Http/Controllers/User/Setup/UserRolesController.php
+++ app/Http/Controllers/User/Setup/UserRolesController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\user\setup;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class UserRolesController extends Controller
 {
-    //
+    public function index(): Factory|View|Application
+    {
+        $users_count = User::where('created_by', '!=', 'system')->count() ?? 0;
+        return view('user.setup.users', compact('users_count'));
+    }
 }

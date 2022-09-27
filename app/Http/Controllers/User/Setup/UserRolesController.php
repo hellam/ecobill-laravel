2f396287ab33fe39661e3ce18fa5c084c51c1385
Hellix <hellamixon@gmail.com>
2022-09-27 17:10:14 +0300

    v1.0.0

---
+++ app/Http/Controllers/User/Setup/UserRolesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\user\setup;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserRolesController extends Controller
+{
+    //
+}

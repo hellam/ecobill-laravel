676e20b101389f15d5fca37ddfe2f1709b5785b3
Hellix <hellamixon@gmail.com>
2022-09-27 18:12:19 +0300

    v1.0.0

--- app/Http/Controllers/User/Setup/UserRolesController.php
+++ app/Http/Controllers/User/Setup/UserRolesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User\Setup;
 
 use App\Http\Controllers\Controller;
+use App\Models\BranchUser;
 use App\Models\User;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
@@ -15,5 +16,35 @@ class UserRolesController extends Controller
     {
         $users_count = User::where('created_by', '!=', 'system')->count() ?? 0;
         return view('user.setup.user_roles', compact('users_count'));
+    }
+
+
+    //Data table API
+    public function dt_api(Request $request): JsonResponse
+    {
+        $users = BranchUser::with(['user','branch','role'])
+            ->orderBy('user', 'desc');
+        return (new DataTables)->eloquent($users)
+            ->addIndexColumn()
+            ->addColumn('id', function ($row) {
+                return ["id" => $row->id, "edit_url" => route('user.setup.users.edit', [$row->id]),
+                    "update_url" => route('user.setup.users.update', [$row->id]),
+//                    "delete_url" => route('user.setup.users.delete', [$row->id])
+                ];
+            })->editColumn('inactive', function ($row) {
+                return $row->inactive == 0 ? '<div class="badge badge-sm badge-light-success">Active</div>' : '<div class="badge badge-sm badge-light-danger">Inactive</div>';
+            })->addColumn('last_visit', function ($row) {
+                $login_log = AuditTrail::where('user', $row->id)
+                    ->where('type', ST_LOGON_EVENT)
+                    ->orderBy('created_at', 'desc')
+                    ->first();
+                return $login_log ? Carbon::parse($login_log->created_at)->format('Y/m/d H:i:s') : 'Never';
+            })
+            ->make(true);
+    }
+
+    public function create(): Factory|View|Application
+    {
+
     }
 }
--- app/Models/BranchUser.php
+++ app/Models/BranchUser.php
@@ -35,4 +35,15 @@ class BranchUser extends Model
 		'branch_id',
         'default'
 	];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function branch(){
+        return $this->belongsTo(Branch::class, 'branch_id', 'id');
+    }
+    public function role(){
+        return $this->belongsTo(Role::class, 'role_id', 'id');
+    }
 }

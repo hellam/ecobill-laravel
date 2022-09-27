2cb9e730c12db63c6dbbcc97b523583d438d8254
Ibrahim <ibrahellamixon@gmail.com>
2022-09-27 17:26:00 +0300

    v1.0.0

--- app/Http/Controllers/User/Setup/UserRolesController.php
+++ app/Http/Controllers/User/Setup/UserRolesController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\user\setup;
 
 use App\Http\Controllers\Controller;
+use App\Models\BranchUser;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class UserRolesController extends Controller
 {
-    //
+    public function index(): Factory|View|Application
+    {
+        $user_roles_count = BranchUser::all() ?? 0;
+        return view('user.setup.user_roles');
+    }
 }

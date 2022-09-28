d729c7ccbb0f5e60829d12391320ae370ec73ab3
Ibrahim <ibrahellamixon@gmail.com>
2022-09-28 14:18:17 +0300

    v1.0.0

--- public/assets/js/pages/business_settings/all.js
+++ public/assets/js/pages/business_settings/all.js
@@ -4,19 +4,21 @@
 const KTBusinessSettingsAll = function () {
     //handle form
     const handleShowResults = function () {
+        getView("business-settings/general_settings/view/general")
+    }
+
+    function getView(url) {
         $.ajax({
             type: 'GET',
-            url: "business-settings/general_settings",
+            url: url,
             success: function (json) {
                 const response = JSON.parse(JSON.stringify(json));
                 $('#loader_container').addClass('d-none')
                 $('#loader_container').after(response)
-
             },
             error: function () {
                 $('#loader_container').addClass('d-none')
                 $('#loader_container').after("Something went wrong! Please try again!")
-
             }
         });
     }

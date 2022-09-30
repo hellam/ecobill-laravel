93a48c32ab87e7fb02201565d0e7cee2c7c7aaca
Hellix <hellamixon@gmail.com>
2022-09-30 16:59:01 +0300

    v1.0.0

--- app/Http/Controllers/User/Banking/GL/GLAccountsController.php
+++ app/Http/Controllers/User/Banking/GL/GLAccountsController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\User\Banking\GL;
 
 use App\Http\Controllers\Controller;
+use App\Models\ChartAccount;
+use App\Models\ChartClass;
+use App\Models\ChartGroup;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -11,9 +14,11 @@ class GLAccountsController extends Controller
 {
     public function index(): Factory|View|Application
     {
-        $gl_accounts_count = 0;
-        $gl_groups_count = 0;
-        $gl_classes_count = 0;
+        $gl_accounts_count = ChartAccount::count() ?? 0;
+        $gl_groups_count = ChartGroup::count() ?? 0;
+        $gl_classes_count = ChartClass::count() ?? 0;
         return view('user.banking_gl.gl_maintenance', compact('gl_accounts_count', 'gl_groups_count', 'gl_classes_count'));
     }
+
+
 }

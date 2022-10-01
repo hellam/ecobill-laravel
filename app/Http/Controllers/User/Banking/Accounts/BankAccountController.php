7f30e547b18a702f553481d558a2d4501078fd30
Ibrahim <ibrahellamixon@gmail.com>
2022-10-01 16:30:50 +0300

    v1.0.0

--- app/Http/Controllers/User/Banking/Accounts/BankAccountController.php
+++ app/Http/Controllers/User/Banking/Accounts/BankAccountController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\User\Banking\Accounts;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 
 class BankAccountController extends Controller
 {
-    public function index()
+    public function index(): Factory|View|Application
     {
         return view('user.banking_gl.accounts.account_maintenance');
     }
-
 
 }

02f04465f7f6d625e5cc1e4f81269102736ecbfc
Hellix <hellamixon@gmail.com>
2022-10-02 21:00:21 +0300

    v1.0.0

--- app/Http/Controllers/User/Banking/CurrencyController.php
+++ app/Http/Controllers/User/Banking/CurrencyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User\Banking;
 
 use App\Http\Controllers\Controller;
+use App\Models\Currency;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -12,6 +13,7 @@ class CurrencyController extends Controller
 {
     public function index(): Factory|View|Application
     {
-        return view('user.banking_gl.currency_fx');
+        $currency_count = Currency::count();
+        return view('user.banking_gl.currency_fx', compact('currency_count'));
     }
 }

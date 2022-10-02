64141a961a4fce70ad3c46bfe7d9826499a18e0d
Ibrahim <ibrahellamixon@gmail.com>
2022-10-02 20:39:56 +0300

    v1.0.0

--- resources/views/layout/user/partials/_side-bar.blade.php
+++ resources/views/layout/user/partials/_side-bar.blade.php
@@ -331,7 +331,7 @@
                     <!--begin:Menu item-->
                     <div class="menu-item">
                         <!--begin:Menu link-->
-                        <a class="menu-link" href="#">
+                        <a class="menu-link @if(Route::is('user.banking.currency.all'))active @endif" href="{{route('user.banking.currency.all')}}">
                                     <span class="menu-bullet">
                                         <span class="bullet bullet-dot"></span>
                                     </span>

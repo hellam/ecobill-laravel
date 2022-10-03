282b753518ebbbb79321ce95714e221c39b999f2
Hellix <hellamixon@gmail.com>
2022-10-03 10:32:45 +0300

    v1.0.0

---
+++ database/migrations/2022_10_03_101734_create_exchange_rate_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exchange_rate', function (Blueprint $table) {
+            $table->id();
+            $table->string('currency',15);
+            $table->double('buy_rate');
+            $table->double('sell_rate');
+            $table->bigInteger('branch');
+            $table->string('client_ref', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exchange_rate');
+    }
+};

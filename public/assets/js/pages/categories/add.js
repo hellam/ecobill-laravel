4a71572ee022e59942abd8d47a724c59ffae081f
Ibrahim <ibrahellamixon@gmail.com>
2022-10-03 22:50:12 +0300

    v1.0.0

--- public/assets/js/pages/categories/add.js
+++ public/assets/js/pages/categories/add.js
@@ -1,17 +1,11 @@
 "use strict";
 
 // Class definition
-var KTModalGroupAdd = function () {
-    var submitButton;
-    var cancelButton;
-    var closeButton;
-    var validator;
-    var form;
-    var modal;
+const KTPayTermsAdd = function () {
+    // Shared variables
+    let closeButton, discardButton, submitButton, validator, form, modal;
 
-    // Init form inputs
-    var handleForm = function () {
-        // Init form validation rules. For more info check the FormValidation plugin's official documentation:https://formvalidation.io/
+    const handleForm = function () {
         validator = FormValidation.formValidation(
             form,
             {
@@ -42,6 +36,7 @@ var KTModalGroupAdd = function () {
             }
         );
 
+
         // Action buttons
         submitButton.addEventListener('click', function (e) {
             e.preventDefault();
@@ -52,106 +47,11 @@ var KTModalGroupAdd = function () {
 
                     if (status === 'Valid') {
                         submitButton.setAttribute('data-kt-indicator', 'on');
-
-                        // Disable submit button whilst loading
-                        submitButton.disabled = true;
-
                         var str = $('#kt_modal_add_category_form').serializeArray().reduce(function (a, x) {
                             a[x.name] = x.value;
                             return a;
                         }, {});
-                        $.ajax({
-                            type: 'POST',
-                            contentType: 'application/json',
-                            url: form.getAttribute("data-kt-action"),
-                            data: JSON.stringify(str),
-                            success: function (json) {
-                                var response = JSON.parse(json);
-                                if (response.status !== true) {
-                                    var errors = response.data;
-                                    for (const [key, value] of Object.entries(errors)) {
-                                        // var field = fields[i];
-                                        // console.log(field.field);
-                                        $('#err_' + key).remove();
-                                        if ($("input[name='" + key + "']").length) {
-                                            $("input[name='" + key + "']")
-                                                .after('<small style="color: red;" id="err_' + key + '">' + value + '</small>')
-                                                .on('keyup', function (e) {
-                                                    $('#err_' + key).remove();
-                                                })
-                                        } else if ($("textarea[name='" + key + "']").length) {
-                                            $("textarea[name='" + key + "']")
-                                                .after('<small style="color: red;" id="err_' + key + '">' + value + '</small>')
-                                                .on('keyup', function (e) {
-                                                    $('#err_' + key).remove();
-                                                })
-                                        }
-                                    }
-
-                                    Swal.fire({
-                                        text: response.message,
-                                        icon: "error",
-                                        buttonsStyling: false,
-                                        confirmButtonText: "Ok!",
-                                        customClass: {
-                                            confirmButton: "btn btn-primary"
-                                        }
-                                    });
-
-                                } else {
-                                    Swal.fire({
-                                        text: response.message,
-                                        icon: "success",
-                                        buttonsStyling: false,
-                                        confirmButtonText: "Ok!",
-                                        customClass: {
-                                            confirmButton: "btn btn-primary"
-                                        }
-                                    }).then(function (result) {
-                                        if (result.isConfirmed) {
-                                            // Hide modal
-                                            modal.hide();
-
-                                            // Enable submit button after loading
-                                            submitButton.disabled = false;
-
-
-                                            form.reset(); // Reset form
-                                            $(".select_contact").val(null).trigger('change');
-
-                                            if ($('#kt_categories_table').length) {
-                                                $("#kt_categories_table").DataTable().ajax.reload();
-                                                return;
-                                            }
-                                            // Redirect to customers list page
-                                            window.location = form.getAttribute("data-kt-redirect");
-                                        }
-                                    });
-                                }
-                                submitButton.removeAttribute('data-kt-indicator');
-
-                                // Enable submit button after loading
-                                submitButton.disabled = false;
-
-                            },
-                            error: function (xhr, desc, err) {
-                                Swal.fire({
-                                    text: 'A network error occured. Please consult your network administrator.',
-                                    icon: "error",
-                                    buttonsStyling: false,
-                                    confirmButtonText: "Ok!",
-                                    customClass: {
-                                        confirmButton: "btn btn-primary"
-                                    }
-                                });
-
-                                submitButton.removeAttribute('data-kt-indicator');
-
-                                // Enable submit button after loading
-                                submitButton.disabled = false;
-
-                            }
-                        });
+                        submitData(str);
                     } else {
                         Swal.fire({
                             text: "Sorry, looks like there are some errors detected, please try again.",
@@ -163,11 +63,11 @@ var KTModalGroupAdd = function () {
                             }
                         });
                     }
-                });
+                })
             }
         });
 
-        cancelButton.addEventListener('click', function (e) {
+        discardButton.addEventListener('click', function (e) {
             e.preventDefault();
 
             Swal.fire({
@@ -184,20 +84,8 @@ var KTModalGroupAdd = function () {
             }).then(function (result) {
                 if (result.value) {
                     form.reset(); // Reset form
-                    $("#select_contact").val(null).trigger('change');
                     modal.hide(); // Hide modal
                 }
-                // else if (result.dismiss === 'cancel') {
-                //     Swal.fire({
-                //         text: "Your form has not been cancelled!.",
-                //         icon: "error",
-                //         buttonsStyling: false,
-                //         confirmButtonText: "Ok, got it!",
-                //         customClass: {
-                //             confirmButton: "btn btn-primary",
-                //         }
-                //     });
-                // }
             });
         });
 
@@ -217,42 +105,142 @@ var KTModalGroupAdd = function () {
                 }
             }).then(function (result) {
                 if (result.value) {
-                    form.reset(); // Reset form
-                    $("#select_contact").val(null).trigger('change');
+                    form.reset(); // Reset formadd_category
                     modal.hide(); // Hide modal
                 }
-                // else if (result.dismiss === 'cancel') {
-                //     Swal.fire({
-                //         text: "Your form has not been cancelled!.",
-                //         icon: "error",
-                //         buttonsStyling: false,
-                //         confirmButtonText: "Ok, got it!",
-                //         customClass: {
-                //             confirmButton: "btn btn-primary",
-                //         }
-                //     });
-                // }
             });
-        })
+        });
     }
 
-    return {
-        // Public functions
-        init: function () {
-            // Elements
-            modal = new bootstrap.Modal(document.querySelector('#kt_modal_add_category'));
+    function submitData(str) {
+        $.ajax({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            type: 'POST',
+            url: form.getAttribute("data-kt-action"),
+            data: str,
+            success: function (json) {
+                var response = JSON.parse(JSON.stringify(json));
+                if (response.status !== true) {
+                    var errors = response.data;
+                    for (const [key, value] of Object.entries(errors)) {
+                        // var field = fields[i];
+                        // console.log(field.field);
+                        $('#err_' + value.field).remove();
+                        $("input[name='" + value.field + "']")
+                            .after('<small style="color: red;" id="err_' + value.field + '">' + value.error + '</small>')
+                            .on('keyup', function (e) {
+                                $('#err_' + value.field).remove();
+                            })
+                    }
 
+                    Swal.fire({
+                        text: response.message,
+                        icon: "error",
+                        buttonsStyling: false,
+                        confirmButtonText: "Ok!",
+                        customClass: {
+                            confirmButton: "btn btn-primary"
+                        }
+                    });
+
+                } else {
+                    Swal.fire({
+                        text: response.message,
+                        icon: "success",
+                        buttonsStyling: false,
+                        confirmButtonText: "Ok!",
+                        customClass: {
+                            confirmButton: "btn btn-primary"
+                        }
+                    }).then(function (result) {
+                        if (result.isConfirmed) {
+                            // Hide modal
+                            modal.hide();
+                            // Enable submit button after loading
+                            submitButton.disabled = false;
+                            form.reset(); // Reset form
+                            window.location.reload();
+                        }
+                    });
+                }
+                submitButton.removeAttribute('data-kt-indicator');
+
+                // Enable submit button after loading
+                submitButton.disabled = false;
+
+            },
+            statusCode: {
+                203: function () {
+                    modal.hide()//hide modal
+                    Swal.fire({
+                        text: "Please provide remarks",
+                        icon: "info",
+                        input: 'textarea',
+                        inputAttributes: {
+                            autocapitalize: 'off'
+                        },
+                        allowOutsideClick: false,
+                        showCancelButton: true,
+                        buttonsStyling: false,
+                        confirmButtonText: "Submit",
+                        cancelButtonText: "Cancel",
+                        // showLoaderOnConfirm: true,
+                        customClass: {
+                            confirmButton: "btn fw-bold btn-danger",
+                            cancelButton: "btn fw-bold btn-active-light-primary"
+                        }
+                    }).then(function (result) {
+                        // delete row data from server and re-draw datatable
+                        if (result.isConfirmed) {
+                            //data.add('remarks', result.value);
+                            // alert(result.value)
+                            modal.show()//show modal
+                            // console.log(str)
+                            // if (result.value)
+                            str = str + "&remarks=" + result.value
+                            submitData(str)
+                        } else {
+                            form.reset(); // Reset form
+                        }
+                    });
+                }
+            },
+            error: function () {
+                Swal.fire({
+                    text: 'A network error occurred. Please consult your network administrator.',
+                    icon: "error",
+                    buttonsStyling: false,
+                    confirmButtonText: "Ok!",
+                    customClass: {
+                        confirmButton: "btn btn-primary"
+                    }
+                });
+
+                submitButton.removeAttribute('data-kt-indicator');
+
+                // Enable submit button after loading
+                submitButton.disabled = false;
+
+            }
+        });
+    }
+
+    // Public methods
+    return {
+        init: function () {
+            modal = new bootstrap.Modal(document.querySelector('#kt_modal_add_category'));
             form = document.querySelector('#kt_modal_add_category_form');
             submitButton = form.querySelector('#kt_modal_add_category_submit');
-            cancelButton = form.querySelector('#kt_modal_add_category_cancel');
+            discardButton = form.querySelector('#kt_modal_add_category_cancel');
             closeButton = form.querySelector('#kt_modal_add_category_close');
-
             handleForm();
         }
-    };
+    }
 }();
 
 // On document ready
 KTUtil.onDOMContentLoaded(function () {
-    KTModalGroupAdd.init();
+    KTPayTermsAdd.init();
 });

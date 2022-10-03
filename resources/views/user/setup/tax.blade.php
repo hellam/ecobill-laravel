90c34ce32865915493f4e5a82d81e3619d7093ab
Ibrahim <ibrahellamixon@gmail.com>
2022-10-03 21:18:59 +0300

    v1.0.0

--- resources/views/user/setup/tax.blade.php
+++ resources/views/user/setup/tax.blade.php
@@ -47,16 +47,16 @@
                     <div class="d-flex align-items-center position-relative my-1">
                         <!--begin::Svg Icon | path: icons/duotune/general/gen021.svg-->
                         <span class="svg-icon svg-icon-1 position-absolute ms-6">
-													<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                         viewBox="0 0 24 24" fill="none">
-														<rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546"
-                                                              height="2" rx="1" transform="rotate(45 17.0365 15.1223)"
-                                                              fill="currentColor"/>
-														<path
-                                                            d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"
-                                                            fill="currentColor"/>
-													</svg>
-												</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                 viewBox="0 0 24 24" fill="none">
+                                <rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546"
+                                      height="2" rx="1" transform="rotate(45 17.0365 15.1223)"
+                                      fill="currentColor"/>
+                                <path
+                                    d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"
+                                    fill="currentColor"/>
+                            </svg>
+                        </span>
                         <!--end::Svg Icon-->
                         <input type="text" data-kt-tax-table-filter="search"
                                class="form-control form-control-solid w-250px ps-15"
@@ -75,13 +75,13 @@
                                 data-kt-menu-placement="bottom-end">
                             <!--begin::Svg Icon | path: icons/duotune/general/gen031.svg-->
                             <span class="svg-icon svg-icon-2">
-													<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                         viewBox="0 0 24 24" fill="none">
-														<path
-                                                            d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z"
-                                                            fill="currentColor"/>
-													</svg>
-												</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                     viewBox="0 0 24 24" fill="none">
+                                    <path
+                                        d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z"
+                                        fill="currentColor"/>
+                                </svg>
+                            </span>
                             <!--end::Svg Icon-->Filter
                         </button>
                         <!--begin::Menu 1-->
@@ -224,7 +224,10 @@
                     </thead>
                     <!--end::Table head-->
                     <!--begin::Table body-->
-                    <tbody class="fw-bold text-gray-600"></tbody>
+                    <tbody class="fw-bold text-gray-600">
+                    @foreach($taxes as $tax)
+                    @endforeach
+                    </tbody>
                     <!--end::Table body-->
                 </table>
                 <!--end::Table-->
@@ -268,7 +271,9 @@
                 <!--begin::Modal content-->
                 <div class="modal-content">
                     <!--begin::Form-->
-                    <form class="form" action="#" id="kt_modal_add_tax_form" data-kt-action="#" data-kt-redirect="">
+                    <form class="form" action="#" id="kt_modal_add_tax_form"
+                          data-kt-action="{{route('user.setup.tax.create')}}"
+                          data-kt-redirect="{{route('user.setup.tax.all')}}">
                         <!--begin::Modal header-->
                         <div class="modal-header" id="kt_modal_add_tax_header">
                             <!--begin::Modal title-->
@@ -279,17 +284,17 @@
                                  class="btn btn-icon btn-sm btn-active-icon-primary">
                                 <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
                                 <span class="svg-icon svg-icon-1">
-															<svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                                                 height="24" viewBox="0 0 24 24" fill="none">
-																<rect opacity="0.5" x="6" y="17.3137" width="16"
-                                                                      height="2" rx="1"
-                                                                      transform="rotate(-45 6 17.3137)"
-                                                                      fill="currentColor"/>
-																<rect x="7.41422" y="6" width="16" height="2" rx="1"
-                                                                      transform="rotate(45 7.41422 6)"
-                                                                      fill="currentColor"/>
-															</svg>
-														</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                         height="24" viewBox="0 0 24 24" fill="none">
+                                        <rect opacity="0.5" x="6" y="17.3137" width="16"
+                                              height="2" rx="1"
+                                              transform="rotate(-45 6 17.3137)"
+                                              fill="currentColor"/>
+                                        <rect x="7.41422" y="6" width="16" height="2" rx="1"
+                                              transform="rotate(45 7.41422 6)"
+                                              fill="currentColor"/>
+                                    </svg>
+                                </span>
                                 <!--end::Svg Icon-->
                             </div>
                             <!--end::Close-->

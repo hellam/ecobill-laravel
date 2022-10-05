a5c3ed63944a05235344f639251e3a7634984100
Hellix <hellamixon@gmail.com>
2022-10-05 12:55:57 +0300

    v1.0.0

---
+++ app/Models/Subscription.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use App\Scopes\ClientRefScope;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * Class Subscription
+ *
+ * @property int $id
+ * @property int $product_id
+ * @property string $name
+ * @property string $image
+ * @property string|null $description
+ * @property string|null $features
+ * @property float|null $price
+ * @property float|null $cost
+ * @property int $validity
+ * @property string|null $client_ref
+ * @property string|null $created_by
+ * @property string|null $updated_by
+ * @property string|null $supervised_by
+ * @property Carbon|null $supervised_at
+ * @property bool|null $inactive
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ *
+ * @package App\Models
+ */
+class Subscription extends Model
+{
+	protected $table = 'subscriptions';
+
+	protected $casts = [
+		'product_id' => 'int',
+		'price' => 'float',
+		'cost' => 'float',
+		'validity' => 'int',
+		'inactive' => 'bool'
+	];
+
+	protected $dates = [
+		'supervised_at'
+	];
+
+	protected $fillable = [
+		'product_id',
+		'name',
+		'image',
+		'description',
+		'features',
+		'price',
+		'cost',
+		'validity',
+		'client_ref',
+		'created_by',
+		'updated_by',
+		'supervised_by',
+		'supervised_at',
+		'inactive'
+	];
+
+
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public static function booted()
+    {
+        if (Auth::guard('user')->check()) {
+            static::addGlobalScope(new ClientRefScope());
+        }
+    }
+}

eb15c7ff953c4dff664be9f815c38d995b9bf56f
Hellix <hellamixon@gmail.com>
2022-10-07 17:08:29 +0300

    v1.0.0

---
+++ app/Http/Controllers/User/Banking/Accounts/BankAccountDeposit.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\User\Banking\Accounts;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BankAccountDeposit extends Controller
+{
+    //
+}

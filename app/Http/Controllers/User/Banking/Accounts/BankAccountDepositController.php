7ce640e92f96493c5bd114e780176ea7b7902057
Ibrahim <ibrahellamixon@gmail.com>
2022-10-07 17:27:41 +0300

    v1.0.0

--- app/Http/Controllers/User/Banking/Accounts/BankAccountDepositController.php
+++ app/Http/Controllers/User/Banking/Accounts/BankAccountDepositController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\User\Banking\Accounts;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class BankAccountDepositController extends Controller
 {
-    //
+    public function index(): Factory|View|Application
+    {
+        return view('user.banking_gl.accounts.bank_deposit');
+    }
 }

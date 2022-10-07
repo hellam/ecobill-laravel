267121a197bb0be9495a0f9c257bac86b015b5cb
Ibrahim <ibrahellamixon@gmail.com>
2022-10-07 14:45:01 +0300

    v1.0.0

--- app/Http/Controllers/User/Customers/CustomerBranchController.php
+++ app/Http/Controllers/User/Customers/CustomerBranchController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\User\Customers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class CustomerBranchController extends Controller
 {
-    //
+    public function index(): Factory|View|Application
+    {
+        return view('users.customers.branch');
+    }
 }

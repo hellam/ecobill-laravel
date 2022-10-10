393c800cd9680a60cabc3e7b46caacc79fee7289
Ibrahim <ibrahellamixon@gmail.com>
2022-10-10 21:18:18 +0300

    v1.0.0

--- resources/views/layout/user/partials/_side-bar.blade.php
+++ resources/views/layout/user/partials/_side-bar.blade.php
@@ -225,7 +225,8 @@
                             <!--begin:Menu item-->
                             <div class="menu-item">
                                 <!--begin:Menu link-->
-                                <a class="menu-link" href="#">
+                                <a class="menu-link @if(Route::is('user.banking_gl.banking.expense'))active @endif"
+                                   href="{{route('user.banking_gl.banking.expense')}}">
                                     <span class="menu-bullet">
                                         <span class="bullet bullet-dot"></span>
                                     </span>

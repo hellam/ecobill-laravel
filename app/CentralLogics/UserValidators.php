b30405fbde092e32db5feda4acfd86a2cac08f59
Hellix <hellamixon@gmail.com>
2022-10-10 12:07:46 +0300

    v1.0.0

--- app/CentralLogics/UserValidators.php
+++ app/CentralLogics/UserValidators.php
@@ -505,7 +505,8 @@ class UserValidators
             'customer_branch_id' => 'required_if:from,1|exists:' . CustomerBranch::class . ',id,client_ref,' . get_user_ref(),
             'into_bank' => 'required|exists:' . BankAccount::class . ',id,client_ref,' . get_user_ref(),
             'fx_rate' => Rule::requiredIf(fn() => (BankAccount::find($request->into_bank)?->currency != session('currency'))),
-            'deposit_options[*]["chat_code"]' => 'required|array|min:1',
+            'deposit_options.*.chat_code' => 'required',
+            'deposit_options.*.amount' => 'required',
         ]);
     }
 

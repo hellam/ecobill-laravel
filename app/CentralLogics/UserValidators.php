6c8795e86a536514f8f1600d338fa529c52ff427
Hellix <hellamixon@gmail.com>
2022-10-10 18:12:25 +0300

    v1.0.0

--- app/CentralLogics/UserValidators.php
+++ app/CentralLogics/UserValidators.php
@@ -514,6 +514,7 @@ class UserValidators
             'deposit_options.*.amount.required' => __('validation.required', ['attribute' => 'Amount']),
             'deposit_options.*.amount.numeric' => __('validation.numeric', ['attribute' => 'Amount']),
             'deposit_options.*.amount.min' => __('validation.min', ['attribute' => 'Amount']),
+            'deposit_options.*' => "At least one deposit option is required",
         ]);
     }
 

f3815f92d783331356c0136d855da944a0c778f9
Hellix <hellamixon@gmail.com>
2022-10-10 11:18:19 +0300

    v1.0.0

--- app/CentralLogics/data_format.php
+++ app/CentralLogics/data_format.php
@@ -16,19 +16,19 @@ function format_date($date,$is_timestamp=false){
     if ($is_timestamp)
         $format =$format.' H:m:s';
 
-    return Carbon::parse($date)->isoFormat($format);
+    return Carbon::parse($date)->format($format);
 }
 
 function get_date_format($is_timestamp=false){
     $format = get_company_setting('date_format');
     $sep = get_company_setting('date_sep');
 
-    if($format=='DDMMYYYY')
-        $format = 'DD'.$sep.'MM'.$sep.'YYYY';
-    elseif($format=='MMDDYYYY')
-        $format = 'MM'.$sep.'DD'.$sep.'YYYY';
-    elseif($format=='YYYYMMDD')
-        $format = 'YYYY'.$sep.'MMM'.$sep.'DD';
+    if($format=='dmY')
+        $format = 'd'.$sep.'m'.$sep.'Y';
+    elseif($format=='mdY')
+        $format = 'm'.$sep.'d'.$sep.'Y';
+    elseif($format=='Ymd')
+        $format = 'Y'.$sep.'m'.$sep.'d';
 
     if ($is_timestamp)
         $format =$format.' H:m:s';
